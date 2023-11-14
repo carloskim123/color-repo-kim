ef34e7888d23a09ef06dbae410b39c930259f07b
Carlos Kirui <carloskirui154@gmail.com>
2023-11-14 15:14:01 -0500

    changed name

---
+++ color-repo-kim/color-repo-kim.js
@@ -0,0 +1,23 @@
+import { convertModule } from "../modules/convert";
+import { infoModule } from "../modules/info";
+import { manipulateModule } from "../modules/manipulate";
+import { parseModule } from "../modules/parse";
+import { validateModule } from "../modules/validate";
+
+export const ColorRepoKim = {
+    convert: convertModule,
+    info: infoModule,
+    parse: parseModule,
+    validate: validateModule,
+    manipulate: manipulateModule
+}
+
+
+module.exports = {
+    ColorRepoKim,
+    infoModule,
+    parseModule,
+    validateModule,
+    convertModule,
+    manipulateModule,
+}
--- color-repo/jest.config.js
+++
@@ -1,5 +0,0 @@
-// jest.config.js
-module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['./test/**/*.test.js'],
-};
