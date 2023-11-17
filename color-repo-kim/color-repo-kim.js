bdd5243454e44f0f0f5cf2419b367189bb0deb95
Carlos Kirui <carloskirui154@gmail.com>
2023-11-17 15:56:28 -0500

    single module imports: v2.1.2

--- color-repo-kim/color-repo-kim.js
+++ color-repo-kim/color-repo-kim.js
@@ -1,9 +1,10 @@
-import { convertModule } from "../modules/convert";
+import { convertModule, convertModule } from "../modules/convert";
 import { infoModule } from "../modules/info";
 import { manipulateModule } from "../modules/manipulate";
 import { parseModule } from "../modules/parse";
 import { validateModule } from "../modules/validate";
 
+// All module imports
 export const ColorRepoKim = {
     convert: convertModule,
     info: infoModule,
@@ -11,4 +12,11 @@ export const ColorRepoKim = {
     validate: validateModule,
     manipulate: manipulateModule,
 }
+
+// Single module exports
+const convertModule = convertModule;
+const infoModule = infoModule;
+const manipulateModule = manipulateModule;
+const parseModule = parseModule;
+const validateModule = validateModule;
 
